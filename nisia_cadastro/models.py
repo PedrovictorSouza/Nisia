e73f790590d2801d2b9284e33b841b8871d87a60
ericbgauch <ericbgauch@gmail.com>
2019-07-10 12:04:36 -0300

    criar model do cadastro de usuario

--- nisia_cadastro/models.py
+++ nisia_cadastro/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class Registered(models.Model):
+    class Meta:
+        verbose_name_plural = 'Registrados'
+
+    CHOICES_ROLE = [
+        ('A ESTUDANTE', 'A ESTUDANTE'),
+        ('A EDUCADORA', 'A EDUCADORA'),
+        ('A EMPREGADORA', 'A EMPREGADORA'),
+        ('A MENTORA', 'A MENTORA'),
+        ('A PROFISSIONAL', 'A PROFISSIONAL')
+    ]
+    name = models.CharField(max_length=120, verbose_name='Nome')
+    email = models.CharField(max_length=120, verbose_name='Email')
+    role = models.CharField(
+        max_length=50,
+        choices=CHOICES_ROLE,
+        verbose_name='Qual o papel que você desempenha hoje?',
+        default='A ESTUDANTE'
+    )
+    story = models.TextField(blank=True, null=True)
