f00d5f2c88dabf33eec06a3888c0cea9e73fa639
ericbgauch <ericbgauch@gmail.com>
2019-07-24 18:40:27 -0300

    adicionar formulario de inscrição junto com model e endpoint

--- nisia_cadastro/admin.py
+++ nisia_cadastro/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from .models import Registered
 
 class RegisteredAdmin(admin.ModelAdmin):
-    list_display = ('name', 'email', 'role')
+    list_display = ('name', 'email', 'role', 'registered_at')
 
 admin.site.register(Registered, RegisteredAdmin)
--- nisia_cadastro/views.py
+++ nisia_cadastro/views.py
@@ -1,7 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
 from nisia_cadastro.models import Registered
 from nisia_cadastro.form import RegisteredForm
+import json
 
 def index(request):
     return render(request, 'index.html',
         {'form': RegisteredForm})
+
+def form(request):
+    msg = {}
+    if request.method == 'POST':
+        register_form = RegisteredForm(json.loads(request.body))
+        if register_form.is_valid():
+            registered_person = Registered()
+            registered_person.name = register_form.cleaned_data['name']
+            registered_person.email = register_form.cleaned_data['email']
+            registered_person.role = register_form.cleaned_data['role']
+            registered_person.story = register_form.cleaned_data['story']
+            registered_person.save()
+            msg = {"result": "Usuário Registrado"}
+            return HttpResponse(msg, status=200)
+        else:
+            msg = {"result": "Dados inválidos"}
+            return HttpResponse(msg, status=400)
+    else:
+        msg = {"result": "Método inválido"}
+        return HttpResponse(msg, status=500)
