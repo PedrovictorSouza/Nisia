47e677a25af9708527be8a832daf8cc983a2ec18
diandra <di.andrade90@gmail.com>
2020-02-20 10:08:18 -0300

    Add filters in Admin for calendar

--- nisia_cadastro/admin.py
+++ nisia_cadastro/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.utils.translation import gettext_lazy as _
-from .models import Registered
+from .models import Registered, Calendar
 from .export import ExportCsvMixin
 
 
@@ -26,6 +26,12 @@ class RegisteredAdmin(admin.ModelAdmin, ExportCsvMixin):
     list_display = ('name', 'email', 'role', 'has_written_story', 'registered_at')
     list_filter = ('role', HasWrittenStoryListFilter, 'registered_at')
     actions = ["export_as_csv"]
+    
+class CalendarAdmin(admin.ModelAdmin, ExportCsvMixin):
+    list_display = ('title', 'date_event', 'start_time', 'type_event', 'is_active')
+    list_filter = ('title', 'is_active')
+    actions = ["export_as_csv"]
 
 admin.site.empty_value_display = 'desconhecido'
 admin.site.register(Registered, RegisteredAdmin)
+admin.site.register(Calendar, CalendarAdmin)
